dd38e8344cc065153a745f93361f9c02a8deba7f
Srikanth Bhadragiri <srikanth.bhadragiri@gmail.com>
2023-11-04 22:26:57 +0530

    add multiselect list

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,8 @@ streamlit.text('Shared Data Set')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Add a picklist
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
