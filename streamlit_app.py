67a9eda9b4bde6fac433e15a5db3b6a11ac0e2b7
Srikanth Bhadragiri <srikanth.bhadragiri@gmail.com>
2023-11-04 22:22:33 +0530

    added list from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('Shared Data Set')
 
 import pandas
 
-my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
