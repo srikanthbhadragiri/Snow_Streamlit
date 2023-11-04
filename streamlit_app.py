c210549ef7610bd6232039cb105ec7199a7ef133
Srikanth Bhadragiri <srikanth.bhadragiri@gmail.com>
2023-11-04 22:44:10 +0530

    code for showing only selected list

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 # Add a picklist
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Grapefruit', 'Kiwifruit'])
+#streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Grapefruit', 'Kiwifruit'])
 #streamlit.dataframe(my_fruit_list)
 
 fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Grapefruit', 'Kiwifruit'])
