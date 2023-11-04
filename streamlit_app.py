33ef26aa22c90530febec0be691aa0c63069ef69
Srikanth Bhadragiri <srikanth.bhadragiri@gmail.com>
2023-11-04 21:03:58 +0530

    print statement update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Srikanth's My First Streamlit App")
+streamlit.header('Snowflake Architech Certification - Syallabus')
+streamlit.text('end-to-end Data Flow')
+streamlit.text('Design and Deploy - Data Architecture')
+streamlit.text('Knowledge on third party tools')
+streamlit.text('Shared Data Set')
