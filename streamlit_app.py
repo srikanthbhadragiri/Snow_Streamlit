7b59a609e7fead73d2307e8d6553c30fdb3e8466
Srikanth Bhadragiri <srikanth.bhadragiri@gmail.com>
2023-11-06 23:12:04 +0530

    correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon'
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered',fruit_choice)
 
 fruityvice_response2 = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
